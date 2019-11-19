741492df65b5e6ee8b30636665cfccd821f0919b
cpspalding <54412573+cpspalding@users.noreply.github.com>
2019-11-19 08:17:51 -0500

    Add files via upload

---
+++ Comparing List and Dictionary.py
@@ -0,0 +1,16 @@
+#Below is example of a List and how to alter it
+lst=list()
+lst.append(21)
+lst.append(183)
+print(lst)
+lst[0]=23
+print(lst)
+#Below is example of a Ditionary and how to alter it
+ddd=dict()
+ddd['age']=21
+ddd['course']=182
+print(ddd)
+ddd['age']=23
+print(ddd)
+
+
