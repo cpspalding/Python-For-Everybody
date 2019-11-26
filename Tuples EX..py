12fdac4eb914f165c61341ee3365794a40b6c4cd
cpspalding <54412573+cpspalding@users.noreply.github.com>
2019-11-26 12:45:23 -0500

    Add files via upload

---
+++ Tuples EX..py
@@ -0,0 +1,27 @@
+
+#Tuples start at 0 so lines of code below print 'Joseph"
+a = ('Howard', 'Paul', 'Joseph')
+print(x[2])
+
+
+#prints '(1,9,2)'
+y = (1, 9, 2)
+print(y)
+
+
+#prints '9'
+print(max(y))
+
+
+#prints tuple vertically
+for x in y:
+    print(x)
+
+
+#Would print Patrick
+(a, b) = (8, 'Patrick')
+print(y)
+
+
+#Would print True
+(0, 1, 2) < (5, 1, 2)
